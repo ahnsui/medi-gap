355cf68c12c6e708bf8f9d5a149080a07765e428
ahnsui <ansui218@ewhain.net>
2025-09-07 01:55:38 +0900

    feat: 지역별 평균 연령 데이터 DB 저장

--- data_processing/region.py
+++ data_processing/region.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import urllib.request
 from dotenv import load_dotenv
 import os
 import json
@@ -10,6 +11,24 @@ DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_DATABASE = os.getenv('DB_DATABASE')
+REGION_API_KEY = os.getenv('REGION_API_KEY')
+
+
+# OPEN API로 데이터 불러오기
+base_url = "https://api.odcloud.kr/api/15099157/v1/uddi:99b0fc58-dcac-4041-bb0e-bb3c5d17531d"
+all_data = []
+
+for page in range(1, 5):
+    url = f"{base_url}?page={page}&perPage=1000&serviceKey={REGION_API_KEY}"
+    
+    request = urllib.request.Request(url) 
+    request.add_header('Authorization', REGION_API_KEY)
+    
+    response = urllib.request.urlopen(request)
+    response_body = response.read()
+    
+    data = json.loads(response_body)
+    all_data.extend(data.get("data", []))
 
 # DB 연결
 connection = mysql.connector.connect(
@@ -18,16 +37,13 @@ connection = mysql.connector.connect(
     password=DB_PASSWORD,
     database=DB_DATABASE
 )
-cursor = connection.cursor()
 
-# JSON 읽기
-with open('data/지역별평균연령.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
+cursor = connection.cursor()
 
 # 시군구 단위로 평균 연령 계산
 grouped = defaultdict(list)
 
-for item in data:
+for item in all_data:
     region_name = item.get('시도명')
     region_subname = item.get('시군구명')
     age = float(item.get('전체 평균연령'))

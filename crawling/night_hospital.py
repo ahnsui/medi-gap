1baf06edbcf7e86615ce5a175928fa17fd28573c
ahnsui <ansui218@ewhain.net>
2025-09-07 21:21:37 +0900

    feat: 지역 이름 수정

--- crawling/night_hospital.py
+++ crawling/night_hospital.py
@@ -34,7 +34,23 @@ articles = driver.find_elements(By.TAG_NAME, 'article')
 # DB 저장
 for article in articles:
     hospital_name = article.find_element(By.CSS_SELECTOR, 'span._1m3fjzl3').text
-    region_name = article.find_element(By.CSS_SELECTOR, 'span._1m3fjzl0').text
+    region = article.find_element(By.CSS_SELECTOR, 'span._1m3fjzl0').text.split()
+    if region[0] == '강원도':
+        region_name = '강원'
+    elif region[0] == '경기도':
+        region_name = '경기'
+    elif region[0] == '경상남도':
+        region_name = '경남'
+    elif region[0] == '전라남도':
+        region_name = '전남'
+    elif region[0] == '전라북도':
+        region_name = '전북'
+    elif region[0] == '충청남도':
+        region_name = '충남'
+    elif region[0] == '충청북도':
+        region_name = '충북'
+    else:
+        region_name = region[0]
 
     sql = 'INSERT INTO tbl_night_hospital(REGION_NAME, HOSPITAL_NAME) VALUES(%s, %s)'
     cursor.execute(sql, (region_name, hospital_name))

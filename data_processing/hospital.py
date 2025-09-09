749e16b9ee0d3c83cf99893c0eda821316f3e13e
ahnsui <ansui218@ewhain.net>
2025-09-09 18:29:41 +0900

    feat: 병원정보에 lat, lon 추가

--- data_processing/hospital.py
+++ data_processing/hospital.py
@@ -1,8 +1,7 @@
 import csv
-from common_module import get_db_connection, get_driver, close_db
+from common_module import get_db_connection, close_db
 
 connection, cursor = get_db_connection()
-
 
 with open('data/전국병원정보.csv', 'r', encoding='utf-8') as f:
     data = csv.DictReader(f)
@@ -10,6 +9,12 @@ with open('data/전국병원정보.csv', 'r', encoding='utf-8') as f:
     for item in data:
         hospital_name = item.get('\ufeff요양기관명')
         doctor_num = item.get('총의사수')
+        lat = item.get('좌표(Y)')  # 위도
+        lon = item.get('좌표(X)')  # 경도
+
+        # float 변환
+        lat = float(lat) if lat and lat.strip() else None
+        lon = float(lon) if lon and lon.strip() else None
 
         # 주소
         address = item.get('주소')
@@ -20,16 +25,19 @@ with open('data/전국병원정보.csv', 'r', encoding='utf-8') as f:
         else:
             region = address_list[:2]
         
-        # 끝 글자가 '구'인 경우 추가
-        if address_list[2].endswith('구'):
+        if len(address_list) > 2 and address_list[2].endswith('구'):
             region.append(address_list[2])
         
         region_name = " ".join(region)
         
         sql = """
-            INSERT INTO tbl_hospital (REGION_CODE, HOSPITAL_NAME, DOCTOR_NUM)
-            VALUES ((SELECT REGION_CODE FROM tbl_region R WHERE R.ADDRESS = %s), %s, %s)
+            INSERT INTO tbl_hospital (REGION_CODE, HOSPITAL_NAME, DOCTOR_NUM, LAT, LON)
+            VALUES (
+                (SELECT REGION_CODE FROM tbl_region R WHERE R.ADDRESS = %s),
+                %s, %s, %s, %s
+            )
         """
-        cursor.execute(sql, (region_name, hospital_name, doctor_num))
+        cursor.execute(sql, (region_name, hospital_name, doctor_num, lat, lon))
 
+connection.commit()
 close_db(connection, cursor)
